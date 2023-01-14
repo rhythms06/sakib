8ab4b551ba4791a3a44d296a3e915fd3497abe29
Sakib Rasul <rhythms06@gmail.com>
2023-01-14 19:39:19 -0400

    refactor: Remove unused template CSS and JSX

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
-import Image from 'next/image'
+'use client';
+
 import { Inter } from '@next/font/google'
-import styles from './page.module.css'
+import {useEffect, useState} from "react";
 
 const inter = Inter({ subsets: ['latin'] })
 
